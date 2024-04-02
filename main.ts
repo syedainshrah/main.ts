8e568c9a985444e1511c839425198ea4da00954e
Syedainshrah <161240095+syedainshrah@users.noreply.github.com>
2024-04-02 15:21:35 +0500

    Initial commit

---
+++ main.ts
@@ -0,0 +1,25 @@
+//2.personal messge : store a person name in a veriable
+//let hello = "inshrah";
+//console.log('Hello ${hello}, would you like to learn some typescript today?')
+
+
+//3.name casses store a person name in a veriable , and print that person name in lowercase , upercase and title case
+
+//let firstname: string ="inshrah";
+//console.log(firstname.toLowerCase());
+
+//let secondname: string ="inshrah";
+//console.log(secondname.toUpperCase())
+
+//let thirdname: string = "inshrah"
+//console.log(thirdname.charAt(0).toLowerCase() + thirdname.slice(1).toUpperCase())
+
+//4. famous quote
+//console.log(`tonny robbin once said,"setting goal is the invisible into visible"`)
+
+//let famous_person = "tonny robbin"
+//let quote = "setting goal is the invisible into visible"
+//console.log(`${famous_person} onse said ${quote}`)
+
+let firstname : string = "\t\n inshrah \t\n"
+console.log(firstname.trim());
