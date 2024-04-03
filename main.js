dc6c317c8a8b5afcff0a40b06d2c70dd105062e4
Syedainshrah <161240095+syedainshrah@users.noreply.github.com>
2024-04-04 01:59:39 +0500

    code

--- main.js
+++ main.js
@@ -14,5 +14,50 @@
 //let famous_person = "tonny robbin"
 //let quote = "setting goal is the invisible into visible"
 //console.log(`${famous_person} onse said ${quote}`)
-let firstname = "\t\n inshrah \t\n";
-console.log(firstname.trim());
+//let firstname : string = "\t\n inshrah \t\n"
+//console.log(firstname.trim());
+
+3/april/2024 
+//Q7,Q8
+//console.log(5+3); //addition
+//console.log(10-2); //subtraction
+//console.log(4*2); //multiply 
+//console.log(16 / 2); //devide
+//Q9
+//let favouriteNumber = 2 ;
+//console.log(`My favourite number is ${favouriteNumber}.`)
+//Q10
+//inshrah 3 april 2024
+//let myName : string = "inshrah"
+//console.log(`my name is ${myName}, would you like some typescript today?`)
+//Q11
+//let i;
+//let Names : string[] = ["Inshrah" , "Charlie" , "Alice"];
+//for (i = 0; i < Names.length ; i++)
+//{
+//    console.log(Names[i]);
+//}
+//Q12
+//let names : string[] = ["inshrah" , "charlie" , "alice"];
+//for (let name of names){
+//    console.log(`hello ${names} , would you like to learn python today`)
+//}
+//Q13
+//let transport : string[] = ["honda motorcycle" , "tesla car", "bianchi bicycle"]
+//transport . forEach (transport=> {
+//  console.log(`i would like to own ${transport}`)
+//});
+//Q14
+//let guests = ["sadia", "soniya", "ana"];
+// guests.forEach(guests=>{
+//  console.log(`hello ${guests}, would you like to join me in iftar`)
+// })  ;
+//Q15
+//let unableToAttend = "ana";
+//console.log(`${unableToAttend} cannot make it to iftar.`);
+//let newGuest = "ishrat";
+//console.log(`${unableToAttend} can you like it to make iftar.`);
+// New invitations
+//guests.forEach(guest => {
+//    console.log(`Dear ${guest}, would you like to join me for dinner?`);
+//});
